e85c8248e8bea07016ed9bc50ddf05e1f2cde3a0
Renuka k <164310372+Renuramchandra@users.noreply.github.com>
2024-10-26 17:27:08 +0530

    Update StoreContext.jsx

--- src/context/StoreContext.jsx
+++ src/context/StoreContext.jsx
@@ -7,7 +7,7 @@ import axios from "axios"
   const StoreContextProvider = (props)=>{
 
  const [cartItems,setCartItems] = useState({});
-  const url = "http://localhost:4000"
+  const url = "https://backend-foodcourt-1.onrender.com"
   const [token,setToken] = useState("");
   const [food_list,setFoodList] = useState([])
  
